77b60b5c69864b982df5afd9f3c7cc7ec32a3ac1
aghagor@gmail.com <aghagor@gmail.com>
2018-02-24 19:32:55 +0400

    creating app

---
+++ app/src/main/java/com/example/goro/useractivityrecognition/Constants.java
@@ -0,0 +1,11 @@
+package com.example.goro.useractivityrecognition;
+
+/**
+ * Created by Goro on 24.02.2018.
+ */
+
+public class Constants {
+    public static final String BROADCAST_DEFECTED_ACTIVITY = "activity_intent";
+    static final long DETECTION_INTERNAL_IN_MILLISECONDS = 30 * 1000;
+    public static final int CONFIDENCE = 70;
+}
